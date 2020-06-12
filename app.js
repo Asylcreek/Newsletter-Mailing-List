25d096b47172c241af38214b9f7b86c0f4ddaed2
Boro Omokugbo Joseph <omokugbobr@gmail.com>
2020-06-12 12:01:52 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const superAgent = require("superagent");
+
+//Initialize express
+app = express();
+
+//Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+/////////////////Route controllers
+//Home Page Controller
+const homePage = (req, res) => {
+    res.sendFile(`${__dirname}/signup.html`);
+};
+
+//Sign up controller
+const signup = (req, res) => {
+    //Get input from the user
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
+    //Create an object with the input
+    const data = {
+        members: [{
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName,
+            },
+            email_address: email,
+            status: "subscribed",
+        }, ],
+    };
+
+    //Api authentication
+    const listID = "5883baaa6b";
+    const allocatedServer = 10;
+    const apiKey = "dd71887642f56e1beb777f3eeb0e0077-us10";
+
+    // Use superagent to post data to MaiChimp api
+    superAgent
+        .post(`https://us${allocatedServer}.api.mailchimp.com/3.0/lists/${listID}`)
+        .send(data) // sends a JSON post body
+        .auth("API-Key", apiKey)
+        .end((error, response) => {
+            if (error || response.statusCode !== 200) {
+                return res.sendFile(`${__dirname}/failure.html`);
+            };
+
+            res.sendFile(`${__dirname}/success.html`);
+        });
+
+};
+
+app.get("/", homePage);
+
+app.post("/", signup);
+
+app.post('/failure', (req, res) => {
+    res.redirect('/');
+});
+
+app.listen(process.env.PORT || 4000, () => console.log("Server running on port 4000"));;
